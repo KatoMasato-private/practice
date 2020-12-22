ebe264a9295964cdb66d47ba22856a811f83a731
Masato Kato <masato.kato0604@gmail.com>
2020-12-22 20:11:27 +0900

    initial commit

---
+++ a-tour-of-go/sample.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func add1(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+const (
+	Big = 1 << 100
+
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 1; i < 11; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+}
+
+func main() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
+
+// 23のmaps exerciseから
